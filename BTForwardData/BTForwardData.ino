b40de65d75206a62959437f20cf1935cf539139c
Chien Yu Lin <myislin@gmail.com>
2016-08-19 18:19:39 +0800

    optimize reading data

--- BTForwardData/BTForwardData.ino
+++ BTForwardData/BTForwardData.ino
@@ -1,10 +1,12 @@
 #include <SoftwareSerial.h>
 SoftwareSerial BTSerial(12, 13); // RX | TX
 
-const int dig[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-int readDig = 0;
+const int dig[10] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+int pinNum = 10;
+int readDig[10];
 int count = 0;
 int passData = 0;
+int dataClock = 2; // 5 microseconds
 
 void setup()
 {
@@ -18,20 +20,40 @@ void setup()
   pinMode(dig[7], INPUT);
   pinMode(dig[8], INPUT);
   pinMode(dig[9], INPUT);
+  
   Serial.begin(9600);
   BTSerial.begin(38400);  // HC-05 default speed in AT command more
 } 
 
 void loop()
 { 
+	/*
   count = 0;
   passData = 0;
-  while(count < 10){
+  while(count < pinNum){
     //BTSerial.print(digitalRead(dig[count++]), BIN);
     passData = passData*2 + int(digitalRead(dig[count++]));
   }
+	*/
+  
+  readDig[0] = digitalRead(dig[0]) << 9;
+  readDig[1] = digitalRead(dig[1]) << 8;
+  readDig[2] = digitalRead(dig[2]) << 7;
+  readDig[3] = digitalRead(dig[3]) << 6;
+  readDig[4] = digitalRead(dig[4]) << 5;
+  readDig[5] = digitalRead(dig[5]) << 4;
+  readDig[6] = digitalRead(dig[6]) << 3;
+  readDig[7] = digitalRead(dig[7]) << 2;
+  readDig[8] = digitalRead(dig[8]) << 1;
+  readDig[9] = digitalRead(dig[9]);
+
+  passData = readDig[0] + readDig[1];
+  passData += readDig[2] + readDig[3];
+  passData += readDig[4] + readDig[5];
+  passData += readDig[6] + readDig[7];
+  passData += readDig[8] + readDig[9];
 
   BTSerial.println(passData, DEC);
 
-  delay(100);
+  delayMicroseconds(dataClock);
 }
